9b5197df061ae31cf8cb5a85067502b612c4898b
Max <alphaQu4z4r@gmail.com>
2018-07-16 23:34:51 +0300

    Update simple_monitoring.sh

--- simple_monitoring.sh
+++ simple_monitoring.sh
@@ -7,7 +7,7 @@
 # tail -f /var/log/simple_mon.log
 
 DATE=`date`
-TIME=`{ time -p curl -o /dev/null --silent --head --write-out 'C: status %{http_code} | T: ' https://bot-vk.ru/top; } 2>&1 | grep -E "real|status"`
+TIME=`{ time -p curl -o /dev/null --silent --head --write-out 'C: status %{http_code} | T: ' https://example.com/top; } 2>&1 | grep -E "real|status"`
 LA=`uptime | awk -F':' '{print $5}'`
 CPU=`grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'`
 
